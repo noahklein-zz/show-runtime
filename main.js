828dfc62b97e27929fc1c83e20db887a375dd741
Noah Klein <noahklein94@gmail.com>
2017-05-22 22:56:43 -0400

    now allows multiple words without requiring quotes on the cmd line

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ async function getRuntime(show) {
   const url = `https://api.tvmaze.com/singlesearch/shows?q=${show}&embed=episodes`;
   const res = await fetch(url);
   const json = await res.json();
-  const runtimes = json._embedded.episodes.map(ep => ep.runtime);
-  return sum(runtimes);
+  const { episodes } = json._embedded;
+  return sum(episodes.map(ep => ep.runtime));
 }
 
 function sum(arr) {
   return arr.reduce((acc, next) => acc + next, 0);
 }
 
-getRuntime(process.argv[2]).then(minutes => console.log('minutes long: ', minutes))
+function args() {
+  const [_, __, ...rest] = process.argv;
+  return rest;
+}
+
+const show = args().join(' ');
+getRuntime(show).then(console.log);
